d626324fd992fb58b643f9e698c9cc54743aa18d
rithishcodespace <rithishcodespace@gmail.com>
2026-06-11 21:43:53 +0530

    upded config with backupOptions like compression algorithms, parallel workers, chunkSize, backup retention duration, maxBackupSize etc..

--- src/config/index.ts
+++ src/config/index.ts
@@ -12,11 +12,18 @@ export interface DatabaseConfig {
   password?: string;
   database: string;
   ssl?: boolean;
+  backupOptions?: {
+    compression?: 'gzip' | 'zstd' | 'none'; // gzip or zstd algorithm to compress backupfile, backup.sql -> z.gz, etc..
+    parallel?: number; // no of parallel threads can be used to complete backup work simultaneously
+    chunkSize?: number; // Size of each piece (chunk) processed at a time
+  }
 }
 
 export interface StorageConfig {
   localPath: string;
   tempPath: string;
+  retention?: number; // days to keep backups
+  maxBackupSize?: string; 
 }
 
 export interface LogConfig {
@@ -32,6 +39,11 @@ export interface AppConfig {
   database?: DatabaseConfig;
   storage: StorageConfig;
   logging: LogConfig;
+  backup?: {
+    defaultType: 'full' | 'incremental' | 'differential';
+    defaultCompression: boolean;
+    maxParallelBackups: number
+  }
 }
 
 class ConfigManager {
@@ -41,6 +53,7 @@ class ConfigManager {
   constructor() {
     this.configPath = process.env.CONFIG_PATH || './config.json';
     this.config = this.loadConfig();
+    this.ensureDirectories();
   }
 
   private loadConfig(): AppConfig {
@@ -71,6 +84,8 @@ class ConfigManager {
       storage: {
         localPath: process.env.BACKUP_PATH || './backups/local',
         tempPath: process.env.TEMP_PATH || './tmp',
+        retention: parseInt(process.env.BACKUP_RETENTION_DAYS || '30'),
+        maxBackupSize: process.env.MAX_BACKUP_sIZE || '50GB'
       },
       logging: {
         level: process.env.LOG_LEVEL || 'info',
@@ -78,6 +93,11 @@ class ConfigManager {
         maxFiles: parseInt(process.env.LOG_MAX_FILES || '30'),
         maxSize: process.env.LOG_MAX_SIZE || '20m',
       },
+      backup:{
+        defaultType: 'full',
+        defaultCompression: true,
+        maxParallelBackups: parseInt(process.env.MAX_PARALLEL_BACKUPS || '3'),
+      }
     };
 
     // Merge with custom config
@@ -134,8 +154,22 @@ class ConfigManager {
     dirs.forEach(dir => {
       if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir, { recursive: true });
+        console.log(`Created directory: ${dir}`);
       }
     });
+  }
+
+  updateBackupConfig(options: any): void {
+    if (!this.config.backup) {
+      this.config.backup = {
+        defaultType: 'full',
+        defaultCompression: true,
+        maxParallelBackups: 3,
+      };
+    }
+    
+    Object.assign(this.config.backup, options);
+    this.saveConfig();
   }
 }
 
